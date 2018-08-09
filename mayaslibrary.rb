9d36187e4fb64320cc81dcaa0c4413ccc0e07506
mayawilli <nikeprosandbigbows23@gmail.com>
2018-08-09 18:29:47 +0000

    automatically backed up by learn

---
+++ mayaslibrary.rb
@@ -0,0 +1,17 @@
+  # describe "Music Library" do
+#   before(:each)
+#   @ library = {
+#     :shawn_mendes => "patience" ,
+#     :billie_eilish => "watch" ,
+#     :the_cheetah_girls => "Cinderella"
+#     :the_neighborhood => "SWeater Weather"
+#     :vance_joy => "riptide"
+#     :rico => "icarly"
+#     :blackbear => "i miss old u"
+#   }
+
+def list_artists(@ library)
+    hash.each_key do |key|
+    puts key
+  end
+end
